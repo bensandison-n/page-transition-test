734aaf922f46d9d399cb496448580c403ee3a37e
bensandison-n <ben.sandison@network-n.com>
2023-08-16 11:34:58 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page">
+    <h1>Homepage</h1>
+    <button>
+      <NuxtLink to="/about">About</NuxtLink>
+    </button>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  max-width: 1000px;
+  margin: 50px auto;
+}
+</style>
